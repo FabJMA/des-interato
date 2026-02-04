566a0e7edf5cbfcf608f5bf90913e490496a04bd
Fabi <fabistudy016@gmail.com>
2026-02-03 23:12:45 -0300

    Register modal + display of information
    Modal da tela de cadastro e exibição das informações cadastradas na tela de início

--- projeto/app/modals.tsx
+++ projeto/app/modals.tsx
@@ -1,1 +1,199 @@
+"use client";
+
+import { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
+
+export default function RegisterModal({ open, close, onAddPet }) {
+    if (!open) return null;
+
+    const [type, setType] = useState("Cachorro");
+    const [birth, setBirth] = useState(null);
+
+    const [nome, setNome] = useState("");
+    const [dono, setDono] = useState("");
+    const [telefone, setTelefone] = useState("");
+    const [raca, setRaca] = useState("");
+
+  return (
+    <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
+      <div className="bg-gradient-to-br from-[#011739] to-[#00060f] rounded-xl w-130 h-120 p-6 border-3 border-blue-400">
+        <div className="flex gap-4 mt-7">
+            <img 
+                src="add_circle.svg" 
+                alt="Add"
+                className="bg-gradient-to-br from-[#00a7f4] to-[#0058e2] rounded-full p-2 w-14 ml-7"
+            />
+            <p className="text-[22px] mt-3">
+                <strong>Cadastrar</strong>
+            </p>
+        </div>
+        
+        <div className="flex ml-8 mt-10">
+            <div className="flex flex-col mr-4">
+            <p className="flex text-[14px] mb-1 mt-0.5">
+                <img 
+                    src="pet.svg" 
+                    alt="Pet" 
+                    className="mr-1 "
+                />
+                Nome
+            </p>
+            <input 
+                type="text" 
+                value={nome}
+                onChange={(e) => setNome(e.target.value)}
+                placeholder=" Nome Sobrenome"
+                className="border-2 rounded-md text-[14px] mb-3"
+            />
+            <p className="flex text-[14px] mb-1">
+                <img 
+                    src="user.svg" 
+                    alt="User" 
+                    className="mr-1"
+                />
+                Dono
+            </p>
+            <input 
+                type="text" 
+                value={dono}
+                onChange={(e) => setDono(e.target.value)}
+                placeholder=" Nome Sobrenome"
+                className="border-2 rounded-md text-[14px] mb-3"
+            />
+            <p className="flex text-[14px] mb-1">
+                <img 
+                    src="phone.svg" 
+                    alt="Phone" 
+                    className="mr-1 "
+                />
+                Telefone
+            </p>
+            <input 
+                type="number" 
+                value={telefone}
+                onChange={(e) => setTelefone(e.target.value)}
+                placeholder=" (00) 0 0000-0000"
+                className="border-2 rounded-md text-[14px] mb-3"
+            />
+            </div>
+            <div className="flex flex-col ml-4">
+            <p className="flex text-[14px] mb-1">
+                <img 
+                    src="dna.svg" 
+                    alt="Genetics" 
+                    className="mr-1"
+                />
+                Animal
+            </p>
+            <div className="flex gap-3 mb-3">
+                <button
+                    onClick={() => setType("Cachorro")}
+                    value={type}
+                    onChange={(e) => setType(e.target.value)}
+                    className={`text-[14px] rounded-md border-2 h-6.5 w-21
+                    ${
+                        type === "Cachorro"
+                        ? " text-white border-white font-semibold"
+                        : "text-gray-400"
+                    }`}
+                >
+                    Cachorro
+                </button>
+
+                <button
+                    onClick={() => setType("Gato")}
+                    value={type}
+                    onChange={(e) => setType(e.target.value)}
+                    className={`text-[14px] rounded-md border-2 h-6.5 w-21
+                    ${
+                        type === "Gato"
+                        ? " text-white border-white font-semibold"
+                        : "text-gray-400"
+                    }`}
+                >
+                    Gato
+                </button>
+            </div>
+            
+            <p className="flex text-[14px] mb-1">
+                <img 
+                    src="dna.svg" 
+                    alt="Genetics" 
+                    className="mr-1"
+                />
+                Raça
+            </p>
+            <input 
+                type="text" 
+                value={raca}
+                onChange={(e) => setRaca(e.target.value)}
+                placeholder=" Raça"
+                className="border-2 rounded-md text-[14px] mb-3"
+            />
+            <p className="flex text-[14px] mb-1">
+                <img 
+                    src="calendar.svg" 
+                    alt="Calendar" 
+                    className="mr-1"
+                />
+                Nascimento <strong className="text-gray-400 ml-1 text-[13px]"> (Aproximado)</strong> 
+            </p>
+                <div className="border-2 rounded-md text-[14px] mb-3">
+                    <DatePicker
+                        selected={birth}
+                        onChange={(birth) => setBirth(birth)}
+                        dateFormat="dd/MM/yyyy"
+                        placeholderText=" 22/08/2020"
+                    />
+                </div>
+            </div>
+        </div>
+
+        <div className="flex items-center mt-13 ml-8">
+            <button 
+                onClick={close}
+                className=" bg-white text-blue-500 rounded-lg flex items-center px-16 h-10 mr-3">
+                <img 
+                    src="arrow_back.svg" 
+                    alt="Back" 
+                    className="mr-1"
+                />
+                Voltar
+            </button>
+            <button 
+                onClick={handleSubmit}
+                className="bg-gradient-to-br from-[#00a7f4] to-[#0058e2] text-white rounded-lg flex items-center px-12 h-10 ml-3">
+                <img 
+                    src="add_circle.svg" 
+                    alt="Add" 
+                    className="mr-1"
+                />
+                Cadastrar
+            </button>
+        </div>
+        
+
+      </div>
+    </div>
+  );
+
+  function handleSubmit() {
+    const pet = {
+        nome,
+        dono,
+        telefone,
+        type,
+        raca,
+        birth: birth?.toLocaleDateString("pt-BR") ?? null,
+
+    };
+
+    onAddPet(pet);
+    close();
+}
+}
+
+
 
--- projeto/app/page.tsx
+++ projeto/app/page.tsx
@@ -1,4 +1,12 @@
+"use client";
+
+import { useState } from "react";
+import CadastrarModal from "./modals";
+
 export default function Home() {
+
+  const [openModal, setOpenModal] = useState(false);
+  const [pets, setPets] = useState([]);
 
   return (
     
@@ -7,10 +15,10 @@ export default function Home() {
         <img
           src="logo.svg"
           alt="Logo"
-          className="w-28 h-auto pt-8 ml-20"
+          className="w-28 h-auto pt-10 ml-20"
         />
 
-        <div className="flex items-start gap-10 mt-4">
+        <div className="flex items-start gap-10 mt-10">
           <div className="relative w-290 ml-20 border-2 rounded-xl h-11 py-2">
             <img 
               src="search.svg" 
@@ -30,6 +38,7 @@ export default function Home() {
           
 
           <button 
+            onClick={() => setOpenModal(true)}
             className="flex items-center gap-2 bg-gradient-to-br from-[#00a7f4] to-[#0058e2] text-white px-4 py-2 rounded-lg font-medium mr-20 hover:bg-blue-600">
             <img
               src="add_circle.svg"
@@ -38,7 +47,51 @@ export default function Home() {
             />
             Cadastrar
           </button>
+          <CadastrarModal
+            open={openModal}
+            close={() => setOpenModal(false)}
+            onAddPet={(pet) => setPets([...pets, pet])}
+          />
         </div>
+        <div className="mt-10 ml-20 mr-20 grid grid-cols-4 gap-6">
+          {pets.map((pet, index) => (
+            <div
+              key={index}
+              className="flex bg-gradient-to-br from-[#001e4d] to-[#0d0014] border-blue-400 rounded-xl p-4 text-white items-center"
+            >
+              <img
+                src={pet.type === "Cachorro" ? "/dog_bone.svg" : "/cat_rat.svg"}
+                alt={pet.type}
+                className="w-20 h-20 bg-gradient-to-br from-[#00a7f4] to-[#0058e2] rounded-full p-2"
+              />
+              
+              <div className="flex flex-col ml-5 ">
+                <div className="flex">
+                  <p className="flex text-[16px] mb-1 mt-0.5  leading-none">
+                    <img 
+                        src="pet.svg" 
+                        alt="Pet" 
+                        className="mr-1 "
+                    />
+                  {pet.nome}
+                  </p>
+                </div>
+                
+                <div className="flex">
+                  <p className="flex text-[16px] mb-1 mt-0.5  leading-none">
+                    <img 
+                        src="user.svg" 
+                        alt="User" 
+                        className="mr-1 "
+                    />
+                  {pet.dono}
+                  </p>
+                </div>
+              </div>
+            </div>
+          ))}
+          </div>
+
       </div>
     </main>
   );
